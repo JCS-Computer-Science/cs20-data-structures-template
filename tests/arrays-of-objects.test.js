6878fb026fdab7b7c8bd83359f505285697f67d8
JoeTodes <jtwilson.home@gmail.com>
2022-02-04 11:10:49 -0700

    objects and arrays of objects tests

---
+++ tests/arrays-of-objects.test.js
@@ -0,0 +1,83 @@
+/*
+- return string of concat-ing all ‘ingredient’ properties of all objects separated by “, “
+- same as above but only if “spicy” prop is false
+- return T/F if “price” of object at specified index is bigger than other
+*/
+const rewire = require("rewire");
+const lib = rewire("../arrays-of-objects.js");
+
+let toppings;
+
+beforeEach(() => {
+	toppings = [
+		{
+			name: "cheese",
+			price: 0.5,
+			spicy: false,
+			inStock: true,
+		},
+		{
+			name: "bacon",
+			price: 1.25,
+			spicy: false,
+			inStock: true,
+		},
+		{
+			name: "sausage",
+			price: 1,
+			spicy: true,
+			inStock: true,
+		},
+		{
+			name: "jalapenos",
+			price: 0.25,
+			spicy: true,
+			inStock: false,
+		},
+		{
+			name: "bbq chicken",
+			price: 2,
+			spicy: false,
+			inStock: false,
+		},
+	];
+});
+
+describe("listAllNames(arr)", () => {
+	let listAllNames;
+	beforeAll(() => {
+		listAllNames = lib.__get__("listAllNames");
+	});
+	it('should return a string combining the "name" property of each object in the array, separated by ", "', () => {
+		let result = listAllNames(toppings);
+		expect(result).toBeDefined();
+		expect(result).toEqual("cheese, bacon, sausage, jalapenos, bbq chicken");
+	});
+});
+
+describe("listFilteredNames(arr, key)", () => {
+	let listFilteredNames;
+	beforeAll(() => {
+		listFilteredNames = lib.__get__("listFilteredNames");
+	});
+	it('should return the same as listAllNames but only including objects with the required "key" having a value of "true"', () => {
+		let result1 = listFilteredNames(toppings, "spicy");
+		expect(result1).toBeDefined();
+		expect(result1).toEqual("sausage, jalapenos");
+		let result2 = listFilteredNames(toppings, "inStock");
+		expect(result2).toBeDefined();
+		expect(result2).toEqual("cheese, bacon, sausage");
+	});
+});
+
+describe("isValueBigger(arr,key,i,j)", () => {
+	let isValueBigger;
+	beforeAll(() => {
+		isValueBigger = lib.__get__("isValueBigger");
+	});
+	it('should return true or false if arr[i] has a larger value for property "key" than arr[j]', () => {
+		expect(isValueBigger(toppings, "price", 0, 1)).toBe(false);
+		expect(isValueBigger(toppings, "price", 4, 2)).toBe(true);
+		expect(isValueBigger(toppings, "price", 3, 3)).toBe(false);
+	});
+});
---
+++ tests/objects.test.js
@@ -0,0 +1,95 @@
+/*
+- return value of specific property
+- add a new property
+- return value of nested property
+- return array of all keys
+- return T/F if specific key exists
+*/
+const rewire = require("rewire");
+const lib = rewire("../objects.js");
+
+let myPokemon;
+
+beforeEach(() => {
+	myPokemon = {
+		name: "Gengar",
+		number: 94,
+		stats: {
+			hp: 60,
+			attack: 65,
+			defense: 60,
+			speed: 110,
+		},
+		evolvesFrom: "Haunter",
+	};
+});
+
+describe("getValue(obj, key)", () => {
+	let getValue;
+	beforeAll(() => {
+		getValue = lib.__get__("getValue");
+	});
+	it("should return the value of property 'key' of 'obj'", () => {
+		expect(getValue(myPokemon, "name")).toEqual("Gengar");
+		expect(getValue(myPokemon, "number")).toEqual(94);
+	});
+});
+
+describe("setValue(obj, key, val)", () => {
+	let setValue;
+	beforeAll(() => {
+		setValue = lib.__get__("setValue");
+	});
+	it("should add a property 'key' with value 'val' to 'obj'", () => {
+		setValue(myPokemon, "color", "purple");
+		expect(myPokemon.color).toBeDefined();
+		expect(myPokemon.color).toEqual("purple");
+	});
+});
+
+describe("getNestedProperty(obj, key1, key2)", () => {
+	let getNestedProperty;
+	beforeAll(() => {
+		getNestedProperty = lib.__get__("getNestedProperty");
+	});
+	it('should return the value of nested property "key2" of property "key1" of "obj"', () => {
+		expect(getNestedProperty(myPokemon, "stats", "hp")).toBeDefined();
+		expect(getNestedProperty(myPokemon, "stats", "hp")).toEqual(60);
+		expect(getNestedProperty(myPokemon, "stats", "speed")).toBeDefined();
+		expect(getNestedProperty(myPokemon, "stats", "speed")).toEqual(110);
+	});
+});
+
+describe("getKeys(obj)", () => {
+	let getKeys;
+	beforeAll(() => {
+		getKeys = lib.__get__("getKeys");
+	});
+	it('should return an array of all the keys in "obj"', () => {
+		expect(getKeys(myPokemon)).toBeDefined();
+		expect(getKeys(myPokemon)).toEqual([
+			"name",
+			"number",
+			"stats",
+			"evolvesFrom",
+		]);
+		expect(getKeys(myPokemon.stats)).toBeDefined();
+		expect(getKeys(myPokemon.stats)).toEqual([
+			"hp",
+			"attack",
+			"defense",
+			"speed",
+		]);
+	});
+});
+
+describe("containsKey(obj, key)", () => {
+	let containsKey;
+	beforeAll(() => {
+		containsKey = lib.__get__("containsKey");
+	});
+	it('should return true or false if "obj" has a property named "key"', () => {
+		expect(containsKey(myPokemon, "evolvesFrom")).toBe(true);
+		expect(containsKey(myPokemon, "evolvesInto")).toBe(false);
+	});
+});
